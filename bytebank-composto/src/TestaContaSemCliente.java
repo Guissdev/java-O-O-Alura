e31eeb0627e5ec50e21f77256778ed69dad5abde
Guilherme Santos <guiss.devs@gmail.com>
2022-12-19 21:19:11 -0300

    Getters e setters de referência

--- bytebank-composto/src/TestaContaSemCliente.java
+++ bytebank-composto/src/TestaContaSemCliente.java
@@ -3,7 +3,7 @@ public class TestaContaSemCliente {
 	
 	public static void main(String[] args) {
 		Conta contaDamarcela = new Conta();
-		System.out.println(contaDamarcela.pegaSaldo());
+		System.out.println(contaDamarcela.getSaldo());
 		
 		contaDamarcela.titular = new Cliente();
 		System.out.println(contaDamarcela.titular);
---
+++ bytebank-encapsulado/src/TestaGetESet.java
@@ -0,0 +1,26 @@
+
+public class TestaGetESet {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		//conta.numero = 1337; (Não compila pois é privado)
+		conta.setNumero(1337);
+		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		//conta.titular = paulo; (Não compila pois é privado)
+		paulo.setNome("Paulo Silveira");
+		
+		conta.setTitular(paulo);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("Programador");
+		//Agora em duas linhas
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("Programador");
+		
+		System.out.println(titularDaConta);
+		System.out.println(paulo);
+		System.out.println(conta.getTitular());
+	} 
+}
